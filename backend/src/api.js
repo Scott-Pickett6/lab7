d48997addff04067d597ddd98f9228ee812dd789
Scott Pickett <sc839110@dal.ca>
2025-03-20 21:40:00 -0300

    backend

---
+++ backend/src/api.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const axios = require("axios");
+const fetch = require("node-fetch");
+
+dotenv.config();
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        message: "test"
+    });
+});
+
+app.use("/.netlify/functions/api", router);
+module.exports = app;
+
+
+module.exports.handler = serverless(app);
